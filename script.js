53fe538114989aace62b9c611414483e4935894b
geoffrey45 <geoffreymungai45@gmail.com>
2021-07-31 13:56:29 +0300

    create displayCart()

--- script.js
+++ script.js
@@ -14,3 +14,9 @@ function createNode(node){
     let element = document.createElement(node);
     return element;
 };
+
+function displayCart(entry, count){
+    let cart_node = createNode("li");
+    cart_node.innerHTML = `${entry}: ${count}`;
+    appendNode(container, cart_node);
+};
